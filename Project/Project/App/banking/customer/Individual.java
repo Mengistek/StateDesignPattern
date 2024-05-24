1273212066f416cd9eeb8b554e0d5175d2631848
Mengistek <116622551+mengistek@users.noreply.github.com>
2024-05-24 18:47:20 -0500

    Initial commit

---
+++ Project/Project/App/banking/customer/Individual.java
@@ -0,0 +1,22 @@
+package app.banking.customer;
+
+
+
+import app.framework.domain.Address;
+import app.framework.domain.Customer;
+
+import java.time.LocalDate;
+
+public class Individual extends Customer {
+
+    private final LocalDate dateOfBirth;
+
+   public Individual(String name, String email, Address address, LocalDate dob){
+        super(name,email,address);
+        this.dateOfBirth = dob;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+}
